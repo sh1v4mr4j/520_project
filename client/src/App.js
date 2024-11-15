d0f9ecd4eee370a990a0f8564daa3e33b9fb8ab1
Shivam Raj <shivamraj@umass.edu>
2024-11-14 22:59:58 -0500

    fixed code

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, {useState} from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import AboutPage from './pages/AboutPage';
 import Navbar from './components/Navbar';
 import PaymentPage from './pages/PaymentPage';
 import DoctorPage from './pages/Doctorpage';
