83da38858bc157ef205a1c388234ac0416302672
A-N1khil <nikhilanand1006@gmail.com>
2024-11-16 03:07:33 -0500

    Removing Mongo URI

--- server/app/services/patient_service.py
+++ server/app/services/patient_service.py
@@ -7,17 +7,6 @@ class PatientService:
     def __init__(self):
         self.uri = os.getenv("MONGO_URI")
         self.client = AsyncIOMotorClient(self.uri)
-    # uri = "mongodb+srv://nikhilanand1006:<db_password>@animus.piv6r.mongodb.net/?retryWrites=true&w=majority&appName=Animus"
-
-    # # Create a new client and connect to the server
-    # client = MongoClient(uri, server_api=ServerApi('1'))
-
-    # # Send a ping to confirm a successful connection
-    # try:
-    #     client.admin.command('ping')
-    #     print("Pinged your deployment. You successfully connected to MongoDB!")
-    # except Exception as e:
-    #     print(e)
 
     def get_config():
         return os.getenv("MY_VAR")
