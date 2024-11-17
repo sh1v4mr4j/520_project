b6f1d0561df478b158861c668a10ed9ee771b15b
A-N1khil <nikhilanand1006@gmail.com>
2024-11-17 02:04:21 -0500

    MapView working

--- client/src/components/MapView.js
+++ client/src/components/MapView.js
@@ -1,15 +1,28 @@
-import React, { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 import mapService from "../services/map-view/map-view-service";
 
 const MapView = () => {
-  const { getGoogleApiKey } = mapService();
-  const [apiKey, setApiKey] = useState(null);
+    const {getGoogleApiKey} = mapService();
+    const [apiKey, setApiKey] = useState(null);
 
-  useEffect(() => {
-    setApiKey(getGoogleApiKey);
-  }, [getGoogleApiKey]);
+    useEffect(() => {
+        setApiKey(getGoogleApiKey);
+    }, [getGoogleApiKey]);
 
-  return <div>{apiKey}</div>;
+    const location_query_str = "Amherst, MA";
+    const location_query = encodeURIComponent(location_query_str);
+    console.log(location_query_str, location_query);
+
+    return (
+        <div>
+            <iframe
+                width="600"
+                height="450"
+                style={{border: 0}}
+                src={`https://www.google.com/maps/embed/v1/place?key=${apiKey}&q=${location_query}`}
+                allowFullScreen />
+        </div>
+    );
 };
 
 export default MapView;
