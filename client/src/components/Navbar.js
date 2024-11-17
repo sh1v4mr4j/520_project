3a62aa07d74126e96f4d1c941da03b73c8046fa6
A-N1khil <nikhilanand1006@gmail.com>
2024-11-17 05:07:07 -0500

    Optimized imports

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React from "react";
 import {Link} from "react-router-dom";
 import {Image, Layout, Menu} from "antd";
 import SchedulCareLogo from "../images/SchedulCareLogo.jpg";
