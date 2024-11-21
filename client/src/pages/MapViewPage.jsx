60d29942d4e4a13299b4a79eea33076f4c6d91ce
A-N1khil <nikhilanand1006@gmail.com>
2024-11-21 02:20:53 -0500

    Merged main, added ss

--- client/src/pages/MapViewPage.jsx
+++ client/src/pages/MapViewPage.jsx
@@ -1,4 +1,4 @@
-import { createElement, useState } from "react";
+import { useState } from "react";
 import { Layout, Menu, theme } from "antd";
 import { Link, Outlet } from "react-router-dom";
 import { Content, Header } from "antd/es/layout/layout";
